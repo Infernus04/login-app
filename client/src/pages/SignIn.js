42013919bb0d0ec07b0a129ab5c07677bc7b139b
Ishan <magardeishan@gmail.com>
2024-05-22 22:34:15 +0530

    complete profile page ui

--- client/src/pages/SignIn.js
+++ client/src/pages/SignIn.js
@@ -63,7 +63,7 @@ const SignIn = () => {
       </form>
       <div className="flex gap-2 mt-5 ">
         <p>Dont Have an account?</p>
-        <Link to="/sign-in">
+        <Link to="/sign-up">
           <span className="text-blue-500">Sign Up</span>
         </Link>
       </div>
